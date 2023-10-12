b0c39ed558b70c051fecfe7eee8a88167762a7c8
Aurore LAFAURIE <aurore31140@gmail.com>
2023-10-12 13:26:04 +0900

    Update of gitignore and barracuda improve

---
+++ Assets/My Scripts/DrawOnTexture.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawOnTexture : MonoBehaviour
+{
+    public Texture2D texture;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoMouseDrawing();
+        
+    }
+
+    private void DoMouseDrawing()
+    {
+        if (Camera.main == null)
+        {
+            throw new System.Exception("Cannot see");
+        }
+
+        //is mouse being pressed
+        if(!Input.GetMouseButton(0) && !Input.GetMouseButton (1))
+        {
+            return;
+        }
+
+        Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hit;
+
+        if (!Physics.Raycast(mouseRay, out hit)) return;
+        if (hit.collider.transform != transform) return;
+
+        Vector2 pixelUV = hit.textureCoord;
+        pixelUV.x *= texture.width;
+        pixelUV.y *= texture.height;
+
+        Color colorToSet = Input.GetMouseButton(0) ? Color.white : Color.black;
+
+        texture.SetPixel((int) pixelUV.x, (int) pixelUV.y, colorToSet);
+        texture.Apply();
+    }
+}
---
+++ Assets/My Scripts/GetInferenceFromDanceModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Barracuda;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GetInferenceFromDanceModel : MonoBehaviour
+{
+    public VideoClip clip;
+    public NNModel modelAsset;
+    private Model _runtimeModel;
+    private IWorker _engine;
+    private int[] ints = new int[2];
+
+    [Serializable]
+    public struct PredictionCategory
+    {
+        public int predictedValue;
+        public float[] predicted;
+
+        public void SetPrediction(Tensor t)
+        {
+            predicted = t.AsFloats();
+            predictedValue = Array.IndexOf(predicted, predicted.Max());
+            Debug.Log($"Predicted {predictedValue} ");
+        }
+    }
+
+    public PredictionCategory prediction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _runtimeModel = ModelLoader.Load(modelAsset);
+
+        _engine = WorkerFactory.CreateWorker(_runtimeModel, WorkerFactory.Device.GPU);
+
+        prediction = new PredictionCategory();
+
+        
+        ints[0] = (int)clip.width;
+        ints[1] = (int)clip.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            // making a tensor out of grayscale texture
+            var channelCount = 1; //grayscale; 3=color; 4=rgba
+            var inputX = new Tensor(1, (int)clip.height, (int)clip.width, channelCount, clip.originalPath);
+
+            Tensor outputY = _engine.Execute(inputX).PeekOutput();
+
+            inputX.Dispose();
+            prediction.SetPrediction(outputY);
+        }
+        
+    }
+
+    private void OnDestroy()
+    {
+        _engine?.Dispose();
+    }
+}
