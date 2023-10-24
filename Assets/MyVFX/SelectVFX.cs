2e1d2dfe3c25e8c2df5d24814b8fa96bc02db139
Aurore LAFAURIE <aurore31140@gmail.com>
2023-10-24 11:42:54 +0900

    :sparkles: Selector of VFX based on random

--- Assets/MyVFX/SelectVFX.cs
+++ Assets/MyVFX/SelectVFX.cs
@@ -2,20 +2,39 @@
 using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.VFX;
 
 public class SelectVFX : MonoBehaviour
 {
-    public VisualEffectAsset[] vfx;
+    public GameObject[] vfx;
+    int currentVFX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var vfx in vfx)
+        {
+            vfx.GetComponent<VisualEffect>().Stop();
+        }
+
+        currentVFX = Random.Range(0, vfx.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int newVFX = Random.Range(0, vfx.Length);
+        if (currentVFX != newVFX)
+        {
+            Debug.Log($"New effect : {newVFX}");
+            SelectionVFX(newVFX, currentVFX);
+            currentVFX = newVFX;
+        }
+    }
+
+    void SelectionVFX(int newVFX, int oldVFX)
+    {
+        vfx[oldVFX].GetComponent<VisualEffect>().Stop();
+        vfx[newVFX].GetComponent<VisualEffect>().Play();
     }
 }
